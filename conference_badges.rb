fc9a0e46e1f67ff65c4b52ad7764349e1b1d8c4a
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-30 03:20:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,16 +8,17 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms(speakers)
-  speakers.map.with_index do |name, index|
+def assign_rooms(attendees)
+  attendees.map.with_index do |name, index|
         "Hello, #{name}! You'll be assigned to room #{index + 1}!"
     end
 end
 
-  def printer(attendees)
-  printer = batch_badge_creator(speakers).concat assign_rooms(speakers)
-  printer.each do |line|
-    puts line
+def printer(attendees)
+  batch_badge_creator(attendees).each do |person|
+    puts person
   end
-  printer(attendees)
+  assign_rooms(attendees).each do |person|
+    puts person
+  end
 end
