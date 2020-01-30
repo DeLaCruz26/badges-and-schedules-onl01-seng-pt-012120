bea6bc79042a7744ab39bf02ae2bb7aea87548e8
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-30 03:10:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,9 +8,9 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_room(speakers)
+def assign_rooms(speakers)
   speakers.map.with_index do |name, index|
-  "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
     end
   end
 
